3938a9286f64868721e7d0a36ae59905faee92d8
xnopre <xnopre@gmail.com>
2016-03-21 11:23:31 +0100

    Nouvelles questions liquidateur : ajout verifs d'enchainements possibles : traitement réponse écran F

--- server/app/utils/engine/QuestionSolverF.java
+++ server/app/utils/engine/QuestionSolverF.java
@@ -1,24 +1,24 @@
 package utils.engine;
 
 import static utils.engine.data.enums.QuestionChoiceValue.getFirstFromJsonArray;
-import static utils.engine.data.enums.RegimeAligne.MSA;
-import static utils.engine.data.enums.UserStatus.STATUS_NSA;
-import static utils.engine.data.enums.UserStatus.STATUS_SA;
+import static utils.engine.data.enums.RegimeAligne.RSI;
+import static utils.engine.data.enums.UserStatus.STATUS_CHEF;
+import static utils.engine.data.enums.UserStatus.STATUS_CONJOINT_COLLABORATEUR;
 
 import utils.engine.data.RegimeLiquidateurAndUserStatus;
 import utils.engine.data.enums.QuestionChoiceValue;
 import utils.engine.data.enums.RegimeAligne;
 
-public class QuestionSolverE implements QuestionSolver {
+public class QuestionSolverF implements QuestionSolver {
 
 	@Override
 	public RegimeLiquidateurAndUserStatus solve(final RegimeAligne[] regimesAlignes, final String liquidateurReponseJsonStr) {
 		final QuestionChoiceValue choiceValue = getFirstFromJsonArray(liquidateurReponseJsonStr);
 		switch (choiceValue) {
 		case OUI:
-			return new RegimeLiquidateurAndUserStatus(MSA, STATUS_NSA);
+			return new RegimeLiquidateurAndUserStatus(RSI, STATUS_CHEF);
 		case NON:
-			return new RegimeLiquidateurAndUserStatus(MSA, STATUS_SA);
+			return new RegimeLiquidateurAndUserStatus(RSI, STATUS_CONJOINT_COLLABORATEUR);
 		}
 		throw new IllegalStateException(
 				"Situation non prévu : liquidateurReponseJsonStr=" + liquidateurReponseJsonStr);
