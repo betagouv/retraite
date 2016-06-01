fb4b507df8df9ed6457aeaab8624bc036c031307
xnopre <xnopre@gmail.com>
2016-06-01 09:27:11 +0200

    Refonte checklist : configuration avec chapitres typés et génération checklist : fin, notamment avec les scripts de migration de la BDD

--- server/db/evolutions/18.sql
+++ server/db/evolutions/18.sql
@@ -2,39 +2,41 @@
 
 SET GLOBAL log_bin_trust_function_creators=1;
 DROP FUNCTION IF EXISTS fnStripTags;
-DELIMITER |
-CREATE FUNCTION fnStripTags( textWithTags varchar(4000) )
-RETURNS varchar(4000)
+CREATE FUNCTION fnStripTags( textToProcess varchar(6000) )
+RETURNS varchar(6000)
 DETERMINISTIC 
 BEGIN
-  DECLARE iStart, iEnd, iLength int;
-  DECLARE tag text;
-    WHILE Locate( '<', textWithTags ) > 0 And Locate( '>', textWithTags, Locate( '<', textWithTags )) > 0 DO
+  DECLARE iStart, iEnd, iLength int;;
+  DECLARE tag text;;
+    SET iStart = 1;;	
+    WHILE Locate( '<', textToProcess, iStart) > 0 And Locate('>', textToProcess, Locate('<', textToProcess, iStart)) > 0 DO
       BEGIN
-        SET iStart = Locate( '<', textWithTags ), iEnd = Locate( '>', textWithTags, Locate('<', textWithTags ));
-        SET iLength = ( iEnd - iStart) + 1;
+        SET iStart = Locate( '<', textToProcess, iStart);;
+        SET iEnd = Locate( '>', textToProcess, Locate('<', textToProcess, iStart));;
+        SET iLength = ( iEnd - iStart) + 1;;
         IF iLength > 0 THEN
           BEGIN
-            SET tag = SUBSTR(textWithTags, iStart, iLength);
+            SET tag = SUBSTR(textToProcess, iStart, iLength);;
             IF tag = '</p>' OR tag = '<br>' OR tag = '<br/>' THEN 
-              SET textWithTags = Insert(textWithTags, iStart, iLength, '\n');
+              SET textToProcess = Insert(textToProcess, iStart, iLength, '</p><p>');;
+              SET iStart = iStart+7;;
             ELSEIF tag = '<li>' THEN 
-              SET textWithTags = Insert(textWithTags, iStart, iLength, '-');
+              SET textToProcess = Insert(textToProcess, iStart, iLength, '</p><p>- ');;
+              SET iStart = iStart+9;;
             ELSE 
-              SET textWithTags = Insert(textWithTags, iStart, iLength, '');
-            END IF;
-          END;
-        END IF;
-      END;
-    END WHILE;
-    RETURN textWithTags;
+              SET textToProcess = Insert(textToProcess, iStart, iLength, '');;
+            END IF;;
+          END;;
+        END IF;;
+      END;;
+    END WHILE;;
+    SET textToProcess = Concat('<p>', textToProcess, '</p>');;
+    RETURN textToProcess;;
 END;
-|
-DELIMITER ;
 
 UPDATE Chapitre set texteActions=fnStripTags(texteIntro);
 UPDATE Chapitre set texteModalites=fnStripTags(parcours) WHERE parcoursDematDifferent=0;
-
+UPDATE Chapitre set texteModalites=CONCAT("*** Parcours canaux traditionnels *** :\n",fnStripTags(parcours),"\n\n*** Parcours dématérialisé *** :\n",fnStripTags(parcoursDemat)) WHERE parcoursDematDifferent=1;
 UPDATE Chapitre set texteInfos=fnStripTags(texteComplementaire);
 
 # --- !Downs
