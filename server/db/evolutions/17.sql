a907453b43fece052f6517c6cb6391306f7c619a
xnopre <xnopre@gmail.com>
2016-06-02 14:56:08 +0200

    Correction script DOWN de migration

--- server/db/evolutions/17.sql
+++ server/db/evolutions/17.sql
@@ -6,6 +6,6 @@ ALTER TABLE Chapitre ADD texteInfos text;
 
 # --- !Downs
 
-ALTER TABLE Chapitre DROP texteIntro;
-ALTER TABLE Chapitre DROP texteModalites;
 ALTER TABLE Chapitre DROP texteInfos;
+ALTER TABLE Chapitre DROP texteModalites;
+ALTER TABLE Chapitre DROP texteActions;
