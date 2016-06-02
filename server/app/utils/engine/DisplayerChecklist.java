6ad7d770b686a6ee999f46dd6564f6f7f6d3198a
xnopre <xnopre@gmail.com>
2016-06-02 11:51:32 +0200

    Correction du test des checklist + correction pour bien prendre en compte les UserStatus (SA, NSA, ...)

--- server/app/utils/engine/DisplayerChecklist.java
+++ server/app/utils/engine/DisplayerChecklist.java
@@ -1,11 +1,8 @@
 package utils.engine;
 
-import static java.util.Arrays.asList;
 import static utils.engine.EngineUtils.firstNotNull;
-import static utils.engine.data.enums.UserStatus.STATUS_CHEF;
 
 import java.util.HashMap;
-import java.util.List;
 
 import controllers.data.PostData;
 import play.Logger;
@@ -16,7 +13,6 @@ import utils.engine.data.RenderData;
 import utils.engine.data.UserChecklistGenerationData;
 import utils.engine.data.enums.Regime;
 import utils.engine.data.enums.RegimeAligne;
-import utils.engine.data.enums.UserStatus;
 import utils.engine.intern.CalculateurRegimeAlignes;
 import utils.engine.intern.UserChecklistGenerationDataBuilder;
 import utils.engine.intern.UserChecklistGenerator;
@@ -41,7 +37,6 @@ public class DisplayerChecklist {
 	public void fillData(final PostData data, final RenderData renderData) {
 		final RegimeAligne[] regimesAlignes = calculateurRegimeAlignes.getRegimesAlignes(data.hidden_regimes);
 		final RegimeAligne regimeLiquidateur = data.hidden_liquidateur;
-		final List<UserStatus> userStatus = asList(STATUS_CHEF);
 		final Regime[] regimes = Regime.fromStringList(data.hidden_regimes);
 		// [XN-29/03/2016-En attendant de remettre les questions complémentaires, on force l'affichage du parcours démat]
 		final boolean isParcoursDemat = true;// questionComplementairesEvaluator.isParcoursDemat(complementReponses);
@@ -51,7 +46,7 @@ public class DisplayerChecklist {
 		final MonthAndYear dateDepart = new MonthAndYear(departMois, departAnnee);
 		final String departement = firstNotNull(data.departement, data.hidden_departement);
 		final UserChecklistGenerationData userChecklistGenerationData = userChecklistGenerationDataBuilder.build(dateDepart, departement,
-				regimes, regimesAlignes, regimeLiquidateur, isParcoursDemat, true, data.hidden_attestationCarriereLongue, userStatus);
+				regimes, regimesAlignes, regimeLiquidateur, isParcoursDemat, true, data.hidden_attestationCarriereLongue, data.hidden_userStatus);
 		renderData.hidden_step = "displayCheckList";
 		try {
 			renderData.userChecklist = userChecklistGenerator.generate(userChecklistGenerationData, regimeLiquidateur);
