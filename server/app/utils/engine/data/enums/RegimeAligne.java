b9a24b9739dad1ce5427309ab0a89464e2096f9f
xnopre <xnopre@gmail.com>
2016-06-20 10:55:58 +0200

    Refonte écrans de sortie : date de départ inconnu

--- server/app/utils/engine/data/enums/RegimeAligne.java
+++ server/app/utils/engine/data/enums/RegimeAligne.java
@@ -2,12 +2,35 @@ package utils.engine.data.enums;
 
 public enum RegimeAligne {
 
-	CNAV(ChecklistName.CNAV), MSA(ChecklistName.MSA), RSI(ChecklistName.RSI);
+	// @formatter:off
+
+	CNAV(ChecklistName.CNAV,
+			"http://CNAV quand-prendre-sa-retraite",
+			"http://CNAV mon-releve-de-carriere",
+			"http://CNAV EVA"),
+
+	MSA(ChecklistName.MSA,
+			"http://MSA quand-prendre-sa-retraite",
+			"http://MSA mon-releve-de-carriere",
+			"http://CNAV MSA"),
+
+	RSI(ChecklistName.RSI,
+			"http://rsi.fr/quand-prendre-sa-retraite",
+			"http://rsi.fr/mon-releve-de-carriere",
+			"http://RSI EVA");
+
+	// @formatter:on
 
 	private final ChecklistName checklistName;
+	public final String urlAgeDepart;
+	public final String urlDroits;
+	public final String urlSimulMontant;
 
-	private RegimeAligne(final ChecklistName checklistName) {
+	private RegimeAligne(final ChecklistName checklistName, final String urlAgeDepart, final String urlDroits, final String urlSimulMontant) {
 		this.checklistName = checklistName;
+		this.urlAgeDepart = urlAgeDepart;
+		this.urlDroits = urlDroits;
+		this.urlSimulMontant = urlSimulMontant;
 	}
 
 	public ChecklistName getChecklistName() {
