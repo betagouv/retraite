8039f033d6b9091f1e635490e51ecd42693aab1d
xnopre <xnopre@gmail.com>
2016-06-21 10:07:50 +0200

    Mise au point script déploiement : vérification de la branche par rapport à l'environnement déployé

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,7 +25,7 @@ fi
 # Vérification que la branche courante est la bonne
 
 CURRENT_GIT_BRANCH=$(git branch | grep ^* | sed 's/* //' )
-if [ ($CURRENT_GIT_BRANCH != $ENV) && ($CURRENT_GIT_BRANCH != "master" || $ENV != "recette") ]; then
+if [[ ($CURRENT_GIT_BRANCH != $ENV) && (($CURRENT_GIT_BRANCH != "master") || ($ENV != "recette")) ]]; then
 	echo
 	echo "La branche GIT courante '$CURRENT_GIT_BRANCH' n'est pas la bonne pour faire le déploiement '$ENV' !"
 	echo
