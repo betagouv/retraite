65ca3037328c31e6ffcdaa268831e30665427a29
xnopre <xnopre@gmail.com>
2016-06-30 15:08:52 +0200

    Renommage de la variable 'regimes_hors_alignes_et_agirc_arrco' en 'regimes_hors_alignes_et_hors_agirc_arrco'

--- server/test/utils/engine/intern/UserChecklistVarsProviderTest.java
+++ server/test/utils/engine/intern/UserChecklistVarsProviderTest.java
@@ -43,7 +43,7 @@ public class UserChecklistVarsProviderTest {
 			{
 				put("regimes_base_hors_alignes", "Banque de France,Caisse d'assurance retraite des chirurgiens dentistes");
 				put("regimes_compl_hors_agirc_arrco", "Caisse nationale de retraite complémentaire des artistes auteurs");
-				put("regimes_hors_alignes_et_agirc_arrco",
+				put("regimes_hors_alignes_et_hors_agirc_arrco",
 						"Caisse nationale de retraite complémentaire des artistes auteurs,Banque de France,Caisse d'assurance retraite des chirurgiens dentistes");
 			}
 		};
