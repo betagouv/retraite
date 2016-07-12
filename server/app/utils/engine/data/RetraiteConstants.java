1a2fb1fcebe3f164a8349233d470bdc4fd0bfdf5
xnopre <xnopre@gmail.com>
2016-07-12 11:02:28 +0200

    Ajout des URLs génériques GIP UR pour les écrans de sortie

--- server/app/utils/engine/data/RetraiteConstants.java
+++ server/app/utils/engine/data/RetraiteConstants.java
@@ -2,8 +2,8 @@ package utils.engine.data;
 
 public interface RetraiteConstants {
 
-	String URL_DROITS_GIP_UR = "http://GIP UR infos droits";
+	String URL_DROITS_GIP_UR = "http://www.info-retraite.fr/mon-parcours-retraite/je-pars-la-retraite/mon-droit-linformation";
 
-	String URL_INFOS_DEPART_RETRAITE_GIP_UR = "http://GIP UR infos départ retraite";
+	String URL_INFOS_DEPART_RETRAITE_GIP_UR = "http://www.info-retraite.fr/mon-parcours-retraite/je-pars-la-retraite/mes-demarches";
 
 }
