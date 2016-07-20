c71165ba7a70b8f3b0f06443ab0bf0be67feac39
xnopre <xnopre@gmail.com>
2016-07-20 16:24:26 +0200

    Gestion des liens avec texte spécifié : gestion des erreurs de syntaxe dans l'édition

--- client/app/src/commons/utils/syntax-analyzer/syntax-analyzer.js
+++ client/app/src/commons/utils/syntax-analyzer/syntax-analyzer.js
@@ -4,26 +4,35 @@ angular.module('SgmapRetraiteCommons').service('SyntaxAnalyzer', function () {
 
     this.isSyntaxError = function (text) {
         if (!text) return false;
-        return isMustacheVarsSyntaxError(text);
+        return isMustacheVarsSyntaxError(text) || isBracketsLinksSyntaxError(text);
     };
 
     function isMustacheVarsSyntaxError(text) {
-        var begin = text.indexOf('{{');
-        var end = text.indexOf('}}');
-        if (begin == -1 && end == -1) {
+        return isSyntaxError(text, '{{', '}}');
+    }
+    
+    function isBracketsLinksSyntaxError(text) {
+        return isSyntaxError(text, '[[', ']]');
+    }
+    
+    function isSyntaxError(text, beginDelimiter, endDelimiter) {
+        var begin = text.indexOf(beginDelimiter);
+        var end = text.indexOf(endDelimiter);
+        if (begin == -1 && end == -1) { 
             return false;
         }
         if (end == -1) {
             return true; 
         }
         if (begin < end) {
-            var beginNext = text.indexOf('{{', begin+2);
+            var beginNext = text.indexOf(beginDelimiter, begin+2);
             if (beginNext != -1 && beginNext < end) {
                 return true;
             }
             var next = text.substr(end+1);
-            return isMustacheVarsSyntaxError(next);
+            return isSyntaxError(next, beginDelimiter, endDelimiter);
         }
         return false;
     }
+    
 });
