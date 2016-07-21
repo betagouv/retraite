087bbc184dbdbe3ccb1628e7881f7e8dbc01e112
xnopre <xnopre@gmail.com>
2016-07-21 15:35:20 +0200

    Modification de présentation de la page de validations des caisses

--- server/app/models/Caisse.java
+++ server/app/models/Caisse.java
@@ -22,4 +22,72 @@ public class Caisse extends RetraiteModel {
 	public String fax;
 
 	public String site;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((adresse1 == null) ? 0 : adresse1.hashCode());
+		result = prime * result + ((adresse2 == null) ? 0 : adresse2.hashCode());
+		result = prime * result + ((adresse3 == null) ? 0 : adresse3.hashCode());
+		result = prime * result + ((adresse4 == null) ? 0 : adresse4.hashCode());
+		result = prime * result + ((fax == null) ? 0 : fax.hashCode());
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		result = prime * result + ((site == null) ? 0 : site.hashCode());
+		result = prime * result + ((telephone == null) ? 0 : telephone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Caisse other = (Caisse) obj;
+		if (adresse1 == null) {
+			if (other.adresse1 != null)
+				return false;
+		} else if (!adresse1.equals(other.adresse1))
+			return false;
+		if (adresse2 == null) {
+			if (other.adresse2 != null)
+				return false;
+		} else if (!adresse2.equals(other.adresse2))
+			return false;
+		if (adresse3 == null) {
+			if (other.adresse3 != null)
+				return false;
+		} else if (!adresse3.equals(other.adresse3))
+			return false;
+		if (adresse4 == null) {
+			if (other.adresse4 != null)
+				return false;
+		} else if (!adresse4.equals(other.adresse4))
+			return false;
+		if (fax == null) {
+			if (other.fax != null)
+				return false;
+		} else if (!fax.equals(other.fax))
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (site == null) {
+			if (other.site != null)
+				return false;
+		} else if (!site.equals(other.site))
+			return false;
+		if (telephone == null) {
+			if (other.telephone != null)
+				return false;
+		} else if (!telephone.equals(other.telephone))
+			return false;
+		return true;
+	}
+
 }
