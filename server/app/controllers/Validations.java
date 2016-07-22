477c7ce6372fa453422e2e74f05c828bc7311c34
xnopre <xnopre@gmail.com>
2016-07-22 17:15:57 +0200

    Ajout écrans pour visualiser tous les écrans de sortie

--- server/app/controllers/Validations.java
+++ server/app/controllers/Validations.java
@@ -6,17 +6,24 @@ import static utils.engine.data.enums.ChecklistName.MSA;
 import static utils.engine.data.enums.ChecklistName.RSI;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import controllers.utils.Look;
 import models.Caisse;
 import models.PeriodeDepartLegal;
+import play.templates.Template;
+import play.templates.TemplateLoader;
 import utils.DateUtils;
 import utils.dao.CaisseDao;
 import utils.dao.PeriodeDepartLegalDao;
 import utils.engine.data.enums.ChecklistName;
+import utils.engine.data.enums.RegimeAligne;
 import utils.engine.utils.AgeLegalEvaluator;
+import utils.wsinforetraite.InfoRetraiteResult.InfoRetraiteResultRegime;
 
 public class Validations extends RetraiteController {
 
@@ -52,7 +59,73 @@ public class Validations extends RetraiteController {
 		render(listPeriodesDepartLegal, listTestsAgeDepart);
 	}
 
+	public static void sorties() {
+
+		final Look look = Look.GENERIC;
+		final List<InfoRetraiteResultRegime> regimesInfos = Arrays.asList(
+				createInfoRetraiteResultRegime(1), createInfoRetraiteResultRegime(2));
+		final Map<String, Object> age = new HashMap<String, Object>() {
+			{
+				put("annees", 60);
+				put("mois", 2);
+			}
+		};
+		final Map<String, Object> ageLegalPourPartir = new HashMap<String, Object>() {
+			{
+				put("annees", 61);
+				put("mois", 4);
+			}
+		};
+		final Map<String, Object> extras = new HashMap<String, Object>() {
+			{
+				put("urlAgeDepart", RegimeAligne.CNAV.urlAgeDepart);
+				put("urlDispositifDepartAvantAgeLegal", RegimeAligne.CNAV.urlDispositifDepartAvantAgeLegal);
+				put("urlDroits", RegimeAligne.CNAV.urlDroits);
+				put("urlSimulMontant", RegimeAligne.CNAV.urlSimulMontant);
+				put("urlInfosDepartRetraite", RegimeAligne.CNAV.urlInfosDepartRetraite);
+				put("urlInfosPenibilite", RegimeAligne.CNAV.urlInfosPenibilite);
+				put("urlSimulMontant", RegimeAligne.CNAV.urlSimulMontant);
+				put("age", age);
+				put("ageLegalPourPartir", ageLegalPourPartir);
+			}
+		};
+		final Map<String, Object> data = new HashMap<String, Object>() {
+			{
+				put("regimesInfos", regimesInfos);
+				put("extras", extras);
+				put("hidden_departMois", "4");
+				put("hidden_departAnnee", "2020");
+			}
+		};
+		final Map<String, Object> args = new HashMap<String, Object>() {
+			{
+				put("data", data);
+				put("look", look);
+				put("withoutPageLayout", true);
+			}
+		};
+
+		String content = "";
+		content += renderEcranSortie("Aucune régime de base aligné", "Application/steps/displaySortieAucunRegimeDeBaseAligne.html", args);
+		content += renderEcranSortie("Date de départ inconnu", "Application/steps/displaySortieDepartInconnu.html", args);
+		content += renderEcranSortie("Pénibilité", "Application/steps/displaySortiePenibilite.html", args);
+		content += renderEcranSortie("Trop jeune", "Application/steps/displaySortieTropJeune.html", args);
+		content += renderEcranSortie("Age < légal - Carrière longue", "Application/steps/displayQuestionCarriereLongue.html", args);
+
+		render(content, look);
+	}
+
 	// Méthodes privées
+
+	private static InfoRetraiteResultRegime createInfoRetraiteResultRegime(final int i) {
+		final InfoRetraiteResultRegime infoRetraiteResultRegime = new InfoRetraiteResultRegime();
+		infoRetraiteResultRegime.regime = "regime " + i;
+		infoRetraiteResultRegime.nom = "nom " + i;
+		infoRetraiteResultRegime.adresse = "adresse " + i;
+		infoRetraiteResultRegime.tel1 = "tel " + i;
+		infoRetraiteResultRegime.email1 = "email " + i;
+		return infoRetraiteResultRegime;
+	}
 
 	private static List<RenderDepartement> createListeDepartements() {
 		final ArrayList<RenderDepartement> departements = new ArrayList<>();
@@ -144,4 +217,15 @@ public class Validations extends RetraiteController {
 		}
 
 	}
+
+	public static String renderEcranSortie(final String title, final String templateName, final Map<String, Object> args) {
+
+		String content = "<h2>" + title + "</h2>";
+
+		final Template template = TemplateLoader.load(template(templateName));
+		content += template.render(args);
+
+		return content;
+	}
+
 }
