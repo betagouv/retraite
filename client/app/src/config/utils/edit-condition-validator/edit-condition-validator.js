facfb71c02344df16f4af63b7a8e46f66e257549
xnopre <xnopre@gmail.com>
2016-08-02 10:20:39 +0200

    Reorganisation des sources et nettoyages

--- client/app/src/config/utils/edit-condition-validator/edit-condition-validator.js
+++ client/app/src/config/utils/edit-condition-validator/edit-condition-validator.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('SgmapRetraiteCommons').service('EditConditionValidator', function ($http) {
+angular.module('SgmapRetraiteConfig').service('EditConditionValidator', function ($http) {
 
     this.isDelaiValide = function(condition) {
         if (condition.props.plusOuMoins && condition.props.nombre && condition.props.unite) {
