ea898fa9ce08798c3a8022f547aeff11b4cfee4d
xnopre <xnopre@gmail.com>
2016-08-02 10:24:56 +0200

    Reorganisation des sources et nettoyages

---
+++ client/app/src/config/utils/object-utils/object-utils.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('SgmapRetraiteConfig').service('ObjectUtils', function () {
+
+    this.equalsIgnoringProperties = function (obj1, obj2, propsToIgnore) {
+        obj1 = angular.copy(obj1);
+        obj2 = angular.copy(obj2);
+        if (propsToIgnore) {
+            deletePropertiesInObject(obj1, propsToIgnore);
+            deletePropertiesInObject(obj2, propsToIgnore);
+        }
+        return angular.equals(obj1, obj2);
+    };
+    
+    function deletePropertiesInObject(obj, propsToIgnore) {
+        propsToIgnore.forEach(function(propToIgnore) {
+            delete obj[propToIgnore];  
+        });
+        for(var prop in obj) {
+            if (startsWith(prop, '$')) {
+                continue;
+            }
+            if (obj.hasOwnProperty(prop)) {
+                var subobj = obj[prop];
+                if (angular.isArray(subobj)) {
+                    angular.forEach(subobj, function(item) {
+                        deletePropertiesInObject(item, propsToIgnore);  
+                    });
+                } else if (subobj instanceof Object) {
+                    deletePropertiesInObject(subobj, propsToIgnore); 
+                }
+            }
+        }
+    }
+
+});
---
+++ client/app/src/config/utils/pasted-text-cleaner/pasted-text-cleaner.spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+describe('PastedTextCleaner', function () {
+
+    beforeEach(module('SgmapRetraiteConfig'));
+
+    var PastedTextCleaner;
+
+    beforeEach(inject(function (_PastedTextCleaner_) {
+        PastedTextCleaner = _PastedTextCleaner_;
+    }));
+
+    describe('replace', function () {
+        
+        it('should clean the diff text', function () {
+
+            var oldText = "<p>qd</p><p>qsd&nbsp;</p><p>azeaze</p><p><br></p>";
+            var newText = "<p>qd</p><p>qsd&nbsp;</p><div class=\"row\"><br>&nbsp;lol&nbsp;&nbsp;&nbsp;aze&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div class=\"col-xs-3\"></div></div><p>azeaze</p><p><br></p>";
+            
+            var result = PastedTextCleaner.clean(oldText, newText);
+
+            var expected = "<p>qd</p><p>qsd&nbsp;</p> lol   aze        <p>azeaze</p><p><br></p>";
+            expect(result).toEqual(expected);
+        });
+        
+    });
+
+});
