d61d7cca3e8161edf70ec2a007428652ae6b0505
xnopre <xnopre@gmail.com>
2016-08-16 13:27:12 +0200

    Correction : message d'erreur si erreur dans la date de naissance saisie

---
+++ server/app/utils/RetraiteBadNaissanceFormatException.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class RetraiteBadNaissanceFormatException extends RetraiteException {
+
+	public RetraiteBadNaissanceFormatException(String message) {
+		super(message);
+
+	}
+
+	public RetraiteBadNaissanceFormatException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+}
--- server/test/utils/engine/utils/AgeCalculatorTest.java
+++ server/test/utils/engine/utils/AgeCalculatorTest.java
@@ -1,10 +1,12 @@
 package utils.engine.utils;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import utils.RetraiteBadNaissanceFormatException;
 import utils.engine.DateProviderFake;
 
 public class AgeCalculatorTest {
@@ -17,10 +19,49 @@ public class AgeCalculatorTest {
 	}
 
 	@Test
-	public void shouldReturnAge() {
+	public void should_return_age() {
 		assertThat(ageCalculator.getAge("25/12/2014")).isEqualTo(0);
 		assertThat(ageCalculator.getAge("22/12/2014")).isEqualTo(1);
 		assertThat(ageCalculator.getAge("12/11/1954")).isEqualTo(61);
+	}
+
+	@Test
+	public void should_throw_exception() {
+		try {
+			ageCalculator.getAge("2512/2014");
+			fail("Devrait déclencher une exception");
+		} catch (final RetraiteBadNaissanceFormatException e) {
+		}
+		try {
+			ageCalculator.getAge("25/122014");
+			fail("Devrait déclencher une exception");
+		} catch (final RetraiteBadNaissanceFormatException e) {
+		}
+		try {
+			ageCalculator.getAge("25122014");
+			fail("Devrait déclencher une exception");
+		} catch (final RetraiteBadNaissanceFormatException e) {
+		}
+		try {
+			ageCalculator.getAge("31/11/2014");
+			fail("Devrait déclencher une exception");
+		} catch (final RetraiteBadNaissanceFormatException e) {
+		}
+		try {
+			ageCalculator.getAge("00/12/2014");
+			fail("Devrait déclencher une exception");
+		} catch (final RetraiteBadNaissanceFormatException e) {
+		}
+		try {
+			ageCalculator.getAge("22/00/2014");
+			fail("Devrait déclencher une exception");
+		} catch (final RetraiteBadNaissanceFormatException e) {
+		}
+		try {
+			ageCalculator.getAge("12/11/0000");
+			fail("Devrait déclencher une exception");
+		} catch (final RetraiteBadNaissanceFormatException e) {
+		}
 	}
 
 }
