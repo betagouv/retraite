8872037e0ca27bb32da98acc817db4f49646d7a6
xnopre <xnopre@gmail.com>
2016-08-16 13:27:12 +0200

    Correction : message d'erreur si erreur dans la date de naissance saisie

--- server/app/utils/engine/utils/AgeCalculator.java
+++ server/app/utils/engine/utils/AgeCalculator.java
@@ -1,7 +1,10 @@
 package utils.engine.utils;
 
 import org.joda.time.DateMidnight;
+import org.joda.time.IllegalFieldValueException;
 import org.joda.time.Years;
+
+import utils.RetraiteBadNaissanceFormatException;
 
 public class AgeCalculator {
 
@@ -13,11 +16,22 @@ public class AgeCalculator {
 
 	public int getAge(final String dateNaissance) {
 		final String[] parts = dateNaissance.split("/");
+		if (parts.length != 3) {
+			throw new RetraiteBadNaissanceFormatException("Mauvais format jj/mm/aaaa pour la date de naissance (" + dateNaissance + ")");
+		}
 		final int day = Integer.parseInt(parts[0]);
 		final int month = Integer.parseInt(parts[1]);
 		final int year = Integer.parseInt(parts[2]);
+		if (year < 1916) {
+			throw new RetraiteBadNaissanceFormatException("Mauvaise années pour la date de naissance (" + dateNaissance + ")");
+		}
 		final DateMidnight today = new DateMidnight(dateProvider.getCurrentDate());
-		final DateMidnight birthday = new DateMidnight(year, month, day);
+		DateMidnight birthday;
+		try {
+			birthday = new DateMidnight(year, month, day);
+		} catch (final IllegalFieldValueException e) {
+			throw new RetraiteBadNaissanceFormatException("Mauvaise date de naissance (" + dateNaissance + ")", e);
+		}
 		return Years.yearsBetween(birthday, today).getYears();
 	}
 
