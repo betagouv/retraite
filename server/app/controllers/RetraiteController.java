51c0f700097549e63836a3621c4cd6625728ef8b
xnopre <xnopre@gmail.com>
2016-08-25 14:42:29 +0200

    GA : désactivation de Google Analytics pour les écrans 'Validations' : 'caisses', 'testsAgeDepart', et 'sorties'

--- server/app/controllers/RetraiteController.java
+++ server/app/controllers/RetraiteController.java
@@ -7,14 +7,23 @@ import utils.JsonUtils;
 
 public class RetraiteController extends Controller {
 
+	private static boolean withoutGoogleAnalytics;
+
 	@Before
 	public static void setGacodeAndAppid() {
-		renderArgs.put("gacode", Play.configuration.getProperty("gacode"));
+		if (!withoutGoogleAnalytics) {
+			renderArgs.put("gacode", Play.configuration.getProperty("gacode"));
+		}
 		renderArgs.put("appid", Play.configuration.getProperty("appid"));
 	}
 
 	protected static void renderJSON(final Object o) {
 		Controller.renderJSON(JsonUtils.toJson(o));
+	}
+
+	protected static void withoutGoogleAnalytics() {
+		withoutGoogleAnalytics = true;
+		renderArgs.put("gacode", null);
 	}
 
 }
