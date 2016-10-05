4f2a5e5f4e6df8380db2b677aef5ced37d05e5e1
Jérôme RIVALS <jerome.rivals@carsat-centreouest.fr>
2016-10-05 16:59:18 +0200

    Migration JBoss : ajout script de déploiement local

---
+++ deploy_jboss.bat
@@ -0,0 +1,5 @@
+cmd /c play war server -o retraite --zip --exclude .sass-cache:.settings:eclipse:javawatch:node_modules:report:test:testjs:tmp
+
+
+
+copy retraite.war C:\usine-dev\workdir\jboss-local\standalone\deployments
