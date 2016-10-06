0bbba18c7231ff327777ad5d68a38404b09afa10
Jérôme RIVALS <jerome.rivals@carsat-centreouest.fr>
2016-10-06 10:22:54 +0200

    Script déploiement JBoss: copie répertoire www de client vers serveur

--- deploy_jboss.bat
+++ deploy_jboss.bat
@@ -1,3 +1,9 @@
+mkdir server\www
+del /S /Q server\www
+
+xcopy /S /Y /Q client\www server\www
+
+
 cmd /c play war server -o retraite --zip --exclude .sass-cache:.settings:eclipse:javawatch:node_modules:report:test:testjs:tmp
 
 
