442c853351498dd449ae048b0197936d36a3838b
xnopre <xnopre@gmail.com>
2016-10-10 10:23:40 +0200

    Affichage des régimes par les variables : ajout de l'acronyme, et ajout d'un espace avant les virgules

--- server/app/utils/engine/intern/UserChecklistVarsProvider.java
+++ server/app/utils/engine/intern/UserChecklistVarsProvider.java
@@ -34,12 +34,16 @@ public class UserChecklistVarsProvider {
 		for (final Regime regime : userChecklistGenerationData.getRegimes()) {
 			if (regime.getType() == BASE_AUTRE) {
 				if (!result.isEmpty()) {
-					result += ",";
+					result += ", ";
 				}
-				result += regime.getNom();
+				result += getNameFor(regime);
 			}
 		}
 		return result;
+	}
+
+	private String getNameFor(final Regime regime) {
+		return regime + " (" + regime.getNom() + ")";
 	}
 
 	private String getRegimeComplHorsAgircArrco(final UserChecklistGenerationData userChecklistGenerationData) {
@@ -47,9 +51,9 @@ public class UserChecklistVarsProvider {
 		for (final Regime regime : userChecklistGenerationData.getRegimes()) {
 			if (regime.getType() == RegimeType.COMPLEMENTAIRE && regime != regime.AGIRC_ARRCO) {
 				if (!result.isEmpty()) {
-					result += ",";
+					result += ", ";
 				}
-				result += regime.getNom();
+				result += getNameFor(regime);
 			}
 		}
 		return result;
@@ -60,9 +64,9 @@ public class UserChecklistVarsProvider {
 		for (final Regime regime : userChecklistGenerationData.getRegimes()) {
 			if (regime.getType() == BASE_AUTRE || (regime.getType() == RegimeType.COMPLEMENTAIRE && regime != regime.AGIRC_ARRCO)) {
 				if (!result.isEmpty()) {
-					result += ",";
+					result += ", ";
 				}
-				result += regime.getNom();
+				result += getNameFor(regime);
 			}
 		}
 		return result;
