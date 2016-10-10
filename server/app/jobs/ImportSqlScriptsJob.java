c0c58bf9650ed43a95f80133f4624f311273ca25
xnopre <xnopre@gmail.com>
2016-10-10 17:40:33 +0200

    Amélioration du job d'import pour prendre tous les fichiers du répertoire sql-imports

--- server/app/jobs/ImportSqlScriptsJob.java
+++ server/app/jobs/ImportSqlScriptsJob.java
@@ -1,6 +1,7 @@
 package jobs;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -16,25 +17,29 @@ import play.jobs.OnApplicationStart;
 @OnApplicationStart
 public class ImportSqlScriptsJob extends Job {
 
-	private static final String DIRNAME = "sql-import";
-	private static final String FILENAME = "sql-scripts-to-import.sql";
+	private static final String DIRNAME = "sql-imports";
 
 	@Override
 	public void doJob() {
-		Logger.info("Current directory : " + new File(".").getAbsolutePath());
 
-		final File file = new File(DIRNAME, FILENAME);
-		if (!file.exists()) {
-			Logger.info("File '" + file.getAbsolutePath() + "' doest not exist : no SQL scripts to import.");
+		final File dirSqlImport = new File(DIRNAME);
+		final File[] files = dirSqlImport.listFiles(new SqlFileFilter());
+		if (files.length == 0) {
+			Logger.info("No .sql files in '" + dirSqlImport.getAbsolutePath() + "' : no SQL scripts to import.");
 			return;
 		}
 
-		Logger.info("File '" + file.getAbsolutePath() + "' exists : try to import SQL scripts...");
-		try {
-			importScripts(file);
-			file.renameTo(new File(DIRNAME, FILENAME + ".imported"));
-		} catch (final Exception e) {
-			Logger.error(e, "Error importing SQL scripts");
+		Logger.info("There " + (files.length == 1 ? "is" : "are") + " " + files.length + " file" + (files.length == 1 ? "" : "s") + " in '"
+				+ dirSqlImport.getAbsolutePath() + "' : try to import SQL scripts...");
+		for (final File file : files) {
+			Logger.info("- Process file " + file.getAbsolutePath() + " ...");
+			try {
+				importScripts(file);
+				file.renameTo(new File(file.getAbsolutePath() + ".imported"));
+				Logger.info("  File " + file.getAbsolutePath() + " processed !");
+			} catch (final Exception e) {
+				Logger.error(e, "Error importing SQL scripts from " + file.getAbsolutePath() + " !");
+			}
 		}
 	}
 
@@ -43,7 +48,7 @@ public class ImportSqlScriptsJob extends Job {
 		for (final String line : lines) {
 			if (isToBeImported(line)) {
 				final String shortline = line.substring(0, Math.min(50, line.length()));
-				Logger.info(". import : " + shortline);
+				Logger.info("  . import : " + shortline);
 				executeSql(line);
 			}
 		}
@@ -77,5 +82,14 @@ public class ImportSqlScriptsJob extends Job {
 
 	private String adaptSql(final String line) {
 		return line.replace("`", "").replace("\\'", "''");
+	}
+
+	private static class SqlFileFilter implements FileFilter {
+
+		@Override
+		public boolean accept(final File pathname) {
+			return pathname.getName().toLowerCase().endsWith(".sql");
+		}
+
 	}
 }
