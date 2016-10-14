f68ba21737c9aeb16722c58ca3a8f77d39ddcff2
Jérôme RIVALS <jerome.rivals@carsat-centreouest.fr>
2016-10-14 14:45:56 +0200

    Utilisation de la librairie javascript html2canvas pour gestion des versions print de tous les écrans et notamment de la checklist

---
+++ server/public/javascripts/printCapture.js
@@ -0,0 +1,31 @@
+"use strict";
+
+$(function() {
+	
+		//Créations des destinées à recevoir les images 
+		//fait avant la capture sinon pb dans l'ordre d'insertion dû à l'asynchonisme de la capture
+		$('.printable').each(function(index) {
+			$('#printContent').append('<div id="imgPrint' + index +'"></div>');
+		});
+
+	
+		$('.printable').each(function(index) {
+			
+			var zone = $(this);
+			html2canvas(zone, {
+				onrendered: function(canvas) {		
+					//Capture de la zone
+					var img = canvas.toDataURL( "image/png" );
+					
+					//Ajout de l'image dans sa div de destination
+					$('#imgPrint'+index).append('<img src="' + img + '" />');
+					
+					//Affichage des boutons masqués de la zone
+					zone.find('.btn').show();
+				},
+				letterRendering: true
+			});
+		});
+			
+});
+
