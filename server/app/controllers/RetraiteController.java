34d73c911b359fca6c5e303571fca0a33253fdfb
xnopre <xnopre@gmail.com>
2016-10-14 15:39:23 +0200

    Correction problème de redirection HTTPS vers HTTP (au lieu de HTTPS)

--- server/app/controllers/RetraiteController.java
+++ server/app/controllers/RetraiteController.java
@@ -3,6 +3,7 @@ package controllers;
 import play.Play;
 import play.mvc.Before;
 import play.mvc.Controller;
+import play.mvc.Http.Header;
 import utils.JsonUtils;
 
 public class RetraiteController extends Controller {
@@ -15,6 +16,20 @@ public class RetraiteController extends Controller {
 			renderArgs.put("gacode", Play.configuration.getProperty("gacode"));
 		}
 		renderArgs.put("appid", Play.configuration.getProperty("appid"));
+	}
+
+	@Before
+	public static void setSecuredAndPortInRequestIfNecessary() {
+
+		// On récupère l'entête perso 'x-forwarded-proto' s'il existe, et s'il vaut 'https'
+		// on modifie les propriétés de 'request' pour que les redirections gérées par
+		// Play! soient bien faites en HTTPS
+
+		final Header xForwardedProtoHeader = request.headers.get("x-forwarded-proto");
+		if (xForwardedProtoHeader != null && "https".equals(xForwardedProtoHeader.value())) {
+			request.secure = true;
+			request.port = 443;
+		}
 	}
 
 	protected static void renderJSON(final Object o) {
