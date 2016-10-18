3e02ddc322f174a4e1fad7018c39432f888d6fe8
xnopre <xnopre@gmail.com>
2016-10-18 09:39:42 +0200

    Mise à jour du test Watai

--- tests_watai/QuestionStep4PageComponent.js
+++
@@ -1,6 +0,0 @@
-title:	          '.contenu h2',
-carriereNonRadio: 'input[name="reponses[CONSULT_RELEVE_CARRIERE]"][value="NON"]',
-    
-checkCarriereNon: function () {
-	return	this.carriereNon()();
-}
--- tests_watai/QuestionStep5PageComponent.js
+++
@@ -1,2 +0,0 @@
-title:	         '.contenu h2',
-paragrapheTitle: 'h3'
