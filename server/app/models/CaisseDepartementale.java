6a8a3bb585357a7bffb7706b43b3cb1e7f353b40
Jérôme RIVALS <jerome.rivals@carsat-centreouest.fr>
2016-11-18 15:32:44 +0100

    ajout du nom des sequences PostGres pour chaque objet Model pour eviter utilisation sequence generique

--- server/app/models/CaisseDepartementale.java
+++ server/app/models/CaisseDepartementale.java
@@ -3,14 +3,24 @@ package models;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 
+import play.db.jpa.GenericModel;
 import utils.engine.data.enums.ChecklistName;
 
 @Entity
-public class CaisseDepartementale extends RetraiteModel {
+public class CaisseDepartementale extends GenericModel {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="caissedepartementale_generator")
+	@SequenceGenerator(name="caissedepartementale_generator", sequenceName="caissedepartementale_id_seq")
+	public Long id;
 
 	@Enumerated(EnumType.STRING)
 	public ChecklistName checklistName;
@@ -20,9 +30,7 @@ public class CaisseDepartementale extends RetraiteModel {
 	@ManyToOne
 	public Caisse caisse;
 
-	@Override
 	public String toString() {
 		return "CaisseDepartementale[id=" + id + ", checklistName=" + checklistName + ", departement=" + departement + ", caisse=" + caisse + "]";
 	}
-
 }
