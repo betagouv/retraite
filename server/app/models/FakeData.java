6bef35626ac9cce3cd84eeba2a255baef904c85e
Jérôme RIVALS <jerome.rivals@carsat-centreouest.fr>
2016-11-18 15:33:18 +0100

    ajout du nom des sequences PostGres pour chaque objet Model pour eviter utilisation sequence generique

--- server/app/models/FakeData.java
+++ server/app/models/FakeData.java
@@ -1,11 +1,22 @@
 package models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import play.db.jpa.GenericModel;
 
 @Entity
-public class FakeData extends RetraiteModel {
+public class FakeData extends GenericModel {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="fakedata_generator")
+	@SequenceGenerator(name="fakedata_generator", sequenceName="fakedata_id_seq")
+	public Long id;
 
 	public String nom;
 	public String nir;
